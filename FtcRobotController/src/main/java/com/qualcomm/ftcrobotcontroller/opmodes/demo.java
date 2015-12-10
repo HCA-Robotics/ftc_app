2eb25184da8f54a5f7e19f5c75d4a3891cef7979
markabbe <markaabbe@gmail.com>
2015-12-10 17:29:22 -0500

    Update demo.java
    Removed threadsleep function from the code. Switched to the encoder using java timer class.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/demo.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/demo.java
@@ -1,18 +1,25 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by Tandi User on 11/13/2015.
  */
-public class demo  extends OpMode {
+public class demo extends OpMode {
 
     DcMotor motorRight;
     DcMotor motorLeft;
+
+    Timer timey = new Timer();
 
     @Override
     public void init() {
@@ -20,34 +27,73 @@ public class demo  extends OpMode {
         motorLeft = hardwareMap.dcMotor.get("motor_1");
     }
 
-    /*@Override public void start() {
+/*    @Override public void start() {
         init();
 
         //waitForStart(); // Wait for the beginning of autonomous phase.
 
 
-    }*/
+    }
+*/
+    boolean hasStarted = false;
+
+    public void initSpeedStuff(){
+        motorRight.setPower(-1.0);
+        motorRight.setTargetPosition(5000);
+        motorLeft.setPower(1.0);
+        motorLeft.setTargetPosition(5000);
+
+        timey.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                checkEncoderDistances();
+            }
+        }, 100);
+    }
+
+    private void checkEncoderDistances(){
+        Log.d("Stuff", String.format("left: %d, right: %d", motorLeft.getCurrentPosition(), motorRight.getCurrentPosition()));
+        if(Math.abs(motorRight.getCurrentPosition()) >= 5000 &&
+           Math.abs(motorLeft.getCurrentPosition()) >= 5000) {
+            motorRight.setPower(0.0);
+            motorLeft.setPower(0.0);
+            return;
+        }
+
+        timey.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                checkEncoderDistances();
+            }
+        }, 100);
+    }
+
 
     @Override
     public void loop() {
-        //OpModeIsActive();
+        //OpModeIsActive;
+        if(!hasStarted){
+            timey.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    initSpeedStuff();
+                }
+            }, 100);
+            hasStarted = true;
+        }
         {
-            motorRight.setPower(-100);
-            motorLeft.setPower(100);
+            /*
+            motorRight.setPower(-1.0);
+            motorRight.setTargetPosition(3000);
+            motorLeft.setPower(1.0);
+            motorLeft.setTargetPosition(3000);
 
-            try {
-                Thread.sleep(3000);
-
-                //setPowerFloat();
-                Thread.sleep(27000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            /*try {
+                Thread.sleep(500);
             }
-
-
-
-
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }*/
+         }
         }
     }
-
-}
